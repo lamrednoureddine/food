9ceafc71cda5b860cf31bd819d5ed654d9c70d6c
noureddine <noureddine.lamred@gmail.com>
2023-12-21 22:50:14 +0100

    adding projet food

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Navb from "./components/Navbar"; 
+import NosService from "./components/NosService "
+import ListeRestaurant from "./components/ListeRestaurant";
+
+// // import Menu from "./components/Menu";
+// import Footer from "./components/Footer";
+const App = () => {
+    return (
+<>
+< Navb/>
+<NosService />
+<ListeRestaurant/>
+{/* <Menu/> */}
+{/* <Footer/> */}
+
+</>
+
+
+    )
+};
+export default App;
+
+
+
+
+
